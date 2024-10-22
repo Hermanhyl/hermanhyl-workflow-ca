bd2e8066e642b4d57d91ced0711b095476a6e085
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-10-22 14:58:44 +0200

    set up e2e testin with cypress for login and logout functions

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,43 @@
+describe(`auth`, () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.wait(500);
+  });
+
+  it(`should able the user to login with valid credentials`, () => {
+    cy.get(`#registerForm`).find(`button[data-auth=login]`).click();
+    cy.wait(500);
+    cy.get(`#loginForm`).within(() => {
+      cy.get(`#loginEmail`).type(`herman@stud.noroff.no`);
+      cy.get(`#loginPassword`).type(`Herman123`);
+
+      cy.get(`button[type="submit"]`).click();
+    });
+  });
+
+  it(`shows an error when trying to login with invalid credentials`, () => {
+    cy.get(`#registerForm`).find(`button[data-auth=login]`).click();
+    cy.wait(500);
+    cy.get(`#loginForm`).within(() => {
+      cy.get(`#loginEmail`).type(`herman@stud.noroff.no`);
+      cy.get(`#loginPassword`).type(`herman321`);
+
+      cy.get(`button[type="submit"]`).click();
+    });
+    cy.on(`window:alert`, (alertText) => {
+      expect(alertText).to.contains("Username or password are incorrect");
+    });
+  });
+
+  it(`should be able to logout`, () => {
+    cy.get(`#registerForm`).find(`button[data-auth=login]`).click();
+    cy.wait(500);
+    cy.get(`#loginForm`).within(() => {
+      cy.get(`#loginEmail`).type(`herman@stud.noroff.no`);
+      cy.get(`#loginPassword`).type(`Herman123`);
+
+      cy.get(`button[type="submit"]`).click();
+    });
+    cy.get(`button[data-auth=logout]`);
+  });
+});
